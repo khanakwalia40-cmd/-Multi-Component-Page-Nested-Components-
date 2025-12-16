6c074b3eaae8455dc489baf2c6e4f512acad1a2e
khanakwalia40-cmd <khanakwalia40@gmail.com>
2025-12-16 22:34:48 +0530

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,18 @@
+// src/Footer.jsx
+function Footer() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#f1f1f1",
+        color: "#555",
+        padding: "8px 16px",
+        fontSize: "14px",
+        textAlign: "center",
+      }}
+    >
+      This is Footer
+    </div>
+  );
+}
+
+export default Footer;
