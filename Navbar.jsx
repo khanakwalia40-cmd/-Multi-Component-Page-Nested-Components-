91aa5c8508ac760bac900a5200acfff7c7fcddea
khanakwalia40-cmd <khanakwalia40@gmail.com>
2025-12-16 22:33:52 +0530

    Create Navbar.jsx

---
+++ Navbar.jsx
@@ -0,0 +1,17 @@
+// src/Navbar.jsx
+function Navbar() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#1976d2",
+        color: "white",
+        padding: "16px",
+        fontSize: "20px",
+      }}
+    >
+      This is Navbar
+    </div>
+  );
+}
+
+export default Navbar;
