0308f88b5551314819abe41feada57f1670f4824
khanakwalia40-cmd <khanakwalia40@gmail.com>
2025-12-16 22:34:19 +0530

    Create Content.jsx

---
+++ Content.jsx
@@ -0,0 +1,16 @@
+// src/Content.jsx
+function Content() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        padding: "40px 16px",
+        fontSize: "18px",
+      }}
+    >
+      This is Main Content
+    </div>
+  );
+}
+
+export default Content;
